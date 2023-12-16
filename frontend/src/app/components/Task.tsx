e0c1f989c06817900075e4c9a1f3ca4f38c26601
Caio Adriano <caiobhadriano@gmail.com>
2023-12-15 23:14:38 -0300

    [REFACTOR]: Create Task Component end importing into TodoList

--- frontend/src/app/components/Task.tsx
+++ frontend/src/app/components/Task.tsx
@@ -1,7 +1,12 @@
+import { TaskInterface } from "@/interfaces/TaskInterface";
 
-
-const Task = (props) => {
-  return <>{props}</>;
+const Task = (task: TaskInterface) => {
+  return (
+    <div>
+      <h1>{task.title}</h1>
+      <p>{task.description}</p>
+    </div>
+  );
 };
 
 export default Task;
