d793b1cf9e1d98d37a1e031f60224ff6ba993d79
Caio Adriano <caiobhadriano@gmail.com>
2023-12-18 15:19:13 -0300

    [FEAT]: Creading new task end returning by id

--- backend/app.py
+++ backend/app.py
@@ -1,37 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
 api = "tarefas"
 
+tasks = [
+    {
+        "id": 1,
+        "title": "Title",
+        "description": "Description",
+        "completed": True,
+    }
+]
 
-@app.route("/tarefas")
+
+@app.route("/tarefas", methods=["GET"])
 def getAllTasks():
-
     # Fazer um Select no BD
-    
-    tasks = [
-        {
-            "id": 1,
-            "title": "Title",
-            "description": "Description",
-            "completed": True,
-        }
-    ]
+    if request.method == 'GET':
+        return make_response(jsonify(tasks), 200)
 
-    return jsonify(tasks)
+
+@app.route("/tarefas", methods=["POST"])
+def createTask():
+    if request.method == 'POST':
+        tasks.append(request.get_json())
+        response = make_response(jsonify(tasks), 201)
+        return response
+
 
 
 @app.route("/tarefas/<int:id_task>")
 def getTaskById(id_task):
-
-    #Fazer um metodo que busca uma task via id no BD
-    return jsonify({
-        "id": id_task,
-        "title": "Task via parametro",
-        "description": "Task Passada via URL",
-        "completed": False,
-    })
+    
+    task = [item for item in tasks if item['id'] == id_task]
+    response = make_response(jsonify(task), 200)
+    return response
 
 
 if __name__ == '__main__':
